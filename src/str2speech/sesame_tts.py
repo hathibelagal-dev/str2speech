03bff77a18b536d289e76e137a75ef70facc4127
Hathibelagal <42626106+hathibelagal-dev@users.noreply.github.com>
2025-03-17 22:13:11 +0100

    hf token

--- src/str2speech/sesame_tts.py
+++ src/str2speech/sesame_tts.py
@@ -8,7 +8,7 @@ class SesameTTS:
         model_path = os.path.join(os.path.expanduser("~"), '.str2speech', 'sesame', "ckpt.pt")
         if not os.path.exists(model_path):
             os.makedirs(os.path.dirname(model_path), exist_ok=True)
-            hf_hub_download(repo_id="sesame/csm-1b", filename=model_path)            
+            hf_hub_download(repo_id="sesame/csm-1b", filename=model_path, token=os.environ.get("HF_TOKEN"))            
         self.model = load_csm_1b(model_path)
         self.voice = None
 
