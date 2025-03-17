fccc4a1297adcdfebf3f60b5015483db94d0723e
Hathibelagal <42626106+hathibelagal-dev@users.noreply.github.com>
2025-03-17 22:19:56 +0100

    HF token handling

--- src/str2speech/sesame_tts.py
+++ src/str2speech/sesame_tts.py
@@ -2,13 +2,23 @@ from .sesame_g import load_csm_1b
 from huggingface_hub import hf_hub_download
 import os
 import torchaudio
+import sys
 
 class SesameTTS:
     def __init__(self):
         model_path = os.path.join(os.path.expanduser("~"), '.str2speech', 'sesame', "ckpt.pt")
         if not os.path.exists(model_path):
             os.makedirs(os.path.dirname(model_path), exist_ok=True)
-            hf_hub_download(repo_id="sesame/csm-1b", filename=model_path, token=os.environ.get("HF_TOKEN"))            
+            hf_token = os.environ.get("HF_TOKEN")
+            if not hf_token:
+                try:
+                    from google.colab import userdata
+                    hf_token = userdata.get('HF_TOKEN')
+                    os.environ['HF_TOKEN'] = hf_token
+                except:
+                    print("HF_TOKEN needs to be set to a valid value.")
+                    sys.exit(2)
+            hf_hub_download(repo_id="sesame/csm-1b", filename=model_path, token=hf_token)            
         self.model = load_csm_1b(model_path)
         self.voice = None
 
