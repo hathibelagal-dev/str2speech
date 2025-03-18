e7bac8276877bf5ace6edb9899a5a4b6e07f6d28
Hathibelagal <42626106+hathibelagal-dev@users.noreply.github.com>
2025-03-18 12:19:15 +0100

    run on gpu and cpu

--- src/str2speech/sesame_tts.py
+++ src/str2speech/sesame_tts.py
@@ -2,6 +2,7 @@ from .sesame_g import load_csm_1b
 from huggingface_hub import hf_hub_download
 import os
 import torchaudio
+import torch
 import sys
 
 class SesameTTS:
@@ -10,7 +11,7 @@ class SesameTTS:
         if not hf_token:
             print("HF_TOKEN is required but not found. Please set it as an environment variable.")
             sys.exit(2)            
-        self.model = load_csm_1b("cuda")
+        self.model = load_csm_1b("cuda" if torch.cuda.is_available() else "cpu")
         self.voice = None
 
     def generate(self, prompt, output_file, sample_rate):
