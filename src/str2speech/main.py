a66ff8b4b814eef8f099a9b79b0bc8fc93aac12d
Hathibelagal <42626106+hathibelagal-dev@users.noreply.github.com>
2025-03-19 16:29:24 +0100

    fix main

--- src/str2speech/main.py
+++ src/str2speech/main.py
@@ -46,8 +46,8 @@ def main():
     if args.list_models:
         print("Available TTS models:")
         i = 0
-        for (_, v) in speaker.Speaker.list_models():
-            print(f"{i}. {v}")
+        for model in speaker.Speaker.list_models():
+            print(f"{i}. {model['name']}")
             i += 1
         sys.exit(0)
 
