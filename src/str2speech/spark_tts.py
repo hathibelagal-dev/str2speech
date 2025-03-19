3740801523d2d7b2c3752f966c9e9f151abbb8ba
Hathibelagal <42626106+hathibelagal-dev@users.noreply.github.com>
2025-03-19 15:40:14 +0100

    choose cli

--- src/str2speech/spark_tts.py
+++ src/str2speech/spark_tts.py
@@ -1,19 +1,13 @@
 from huggingface_hub import snapshot_download
 from .utils import get_downloads_path
 import os
-from transformers import AutoTokenizer, AutoModelForCausalLM
-from sparktts.models.audio_tokenizer import BiCodecTokenizer
-from sparktts.utils.token_parser import LEVELS_MAP, GENDER_MAP, TASK_TOKEN_MAP
+from .base_tts import BaseTTS
 
-class SparkTTS:
+class SparkTTS(BaseTTS):
     model_name = "SparkAudio/Spark-TTS-0.5B"
     def __init__(self):
         super().__init__()
         self.download_model()
-        self.tokenizer = AutoTokenizer.from_pretrained(f"{self.model_dir}/LLM")
-        self.model = AutoModelForCausalLM.from_pretrained(f"{self.model_dir}/LLM")
-        self.audio_tokenizer = BiCodecTokenizer(self.model_dir, device=self.device)
-        self.model.to(self.device)
 
     def download_model(self):
         self.model_dir = get_downloads_path("SparkTTS")
